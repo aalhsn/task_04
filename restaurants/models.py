96848c62d9384122b25336d6538bc041bba1bb17
Abdullah A. Alhasan <a.a.alhasan95@gmail.com>
2019-08-20 16:22:54 +0300

    Abdullah Alhasan & Alyaa Alostad

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Restaurant(models.Model):
-	name = models.CharField(max_length=50)
+	name = models.CharField(max_length=60)
 	description = models.TextField()
 	opening_time = models.TimeField()
 	closing_time = models.TimeField()
